3cfab334ccd666f408b06181a23386dfd8d6d940
aswami318 <aswami318@gmail.com>
2022-10-12 23:47:44 +0530

    Second

---
+++ index.java
@@ -0,0 +1,16 @@
+class output
+{
+ public static void main(String a[])
+{
+ System.out.println("Welcome Dj Swami");
+ int i=1;
+ int n=5;
+ int f=1;
+ for(i=1;i<=n;i++)
+ {
+     f=f*i;
+}
+ 
+ System.out.println("Fact:"+f);
+}
+}
